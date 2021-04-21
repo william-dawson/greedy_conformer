2678920bb69f3dfe97ddb0a7f4f1414ba5a6d907
William Dawson <william.dawson@riken.jp>
2021-04-21 13:32:23 +0900

    Energy filter

--- ReadMe.md
+++ ReadMe.md
@@ -2,14 +2,16 @@
 
 This will greedily prune a set of conformers. To use it:
 ```
-python main.py input 0.1
+python main.py input 0.1 10
 ```
 Inside `input` should be your list of xyz files. The second argument is
-the RMSD value that you use as a tolerance. The output will be written
-to failures.txt and successes.txt. Failures details which file matched
-to something else in the set.
+the RMSD value that you use as a tolerance. The third argument is a relative 
+energy difference value (percent), for which two conformers are assumed to 
+be different if their relative energies are different by that amount or more.
+The output will be written to failures.txt and successes.txt. Failures details
+which file matched to something else in the set.
 
-To use this you need to install the python bindings for openbabel
+To use this you need to install the python bindings for openbabel and tqdm
 (either with pip or conda). The first time you run the script might
-be slow as it JIT compiles the code
+be slow as it JIT compiles the code.
 
--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 Greedily prune a set of conformers.
 
 Usage:
-    python main.py input 0.1
+    python main.py input 0.1 10
 """
 
 
 def read_babel(f):
+    """
+    Read in an xyz file into an openbabel molecule.
+    """
     from openbabel.openbabel import OBMol, OBConversion
 
     conv = OBConversion()
@@ -21,7 +24,21 @@ def read_babel(f):
     return mol
 
 
-def filter(systems):
+def energy(mol):
+    """
+    Compute the energy of a molecule using GAFF.
+    """
+    from openbabel.openbabel import OBForceField
+    ff = OBForceField.FindForceField("gaff")
+    ff.Setup(mol)
+
+    return ff.Energy()
+
+
+def filter(systems, energies, rmsd_cutoff, energy_cutoff):
+    """
+    Filter the systems based on an RMSD criteria.
+    """
     from openbabel.openbabel import OBAlign
 
     failures = {}
@@ -34,11 +51,14 @@ def filter(systems):
         for f2, mol2 in passing.items():
             if f1 == f2:
                 continue
+            ediff = 100 * abs(energies[f1] - energies[f2]) / abs(energies[f1])
+            if ediff > energy_cutoff:
+                continue
             align = OBAlign(mol1, mol2, True, True)
             align.Align()
             score = align.GetRMSD()
-            if score < cutoff:
-                failures[f1 + " " + f2] = score
+            if score < rmsd_cutoff:
+                failures[f1 + "\t" + f2] = str(score)+"\t"+str(ediff)
                 found = True
                 break
         if not found:
@@ -54,10 +74,11 @@ if __name__ == "__main__":
     from tqdm import tqdm
 
     # Get the input path
-    if len(argv) < 3:
-        raise Exception("Requires input path and cutoff")
+    if len(argv) < 4:
+        raise Exception("Requires input path, rmsd cutoff, energy cutoff")
     flist = glob(join(argv[1], "*"))
-    cutoff = float(argv[2])
+    rmsd_cutoff = float(argv[2])
+    energy_cutoff = float(argv[3])
 
     # Read in the systems
     print("Reading in systems")
@@ -66,16 +87,28 @@ if __name__ == "__main__":
         f = flist[i]
         systems[f] = read_babel(f)
 
+    # Compute the energies
+    print("Computing Energies")
+    energies = {}
+    for f in tqdm(list(systems)):
+        energies[f] = energy(systems[f])
+
     # Filter
     print("Filtering")
-    passing, failures = filter(systems)
+    passing, failures = filter(systems, energies, rmsd_cutoff, energy_cutoff)
 
     print("Writing Output")
     # Write out the failures
     with open(join("failures.txt"), "w") as ofile:
+        ofile.write("System 1\tSystem 2\tRMSD\tEnergy Difference (%)\n")
         for k, rmsd in failures.items():
             ofile.write(k + "\t" + str(rmsd) + "\n")
 
     # Write out the successes
     with open(join("successes.txt"), "w") as ofile:
         ofile.write("\n".join(list(passing)))
+
+    # Write out the energies
+    with open(join("energies.txt"), "w") as ofile:
+        for k, ene in energies.items():
+            ofile.write(k + "\t" + str(ene) + "\n")
